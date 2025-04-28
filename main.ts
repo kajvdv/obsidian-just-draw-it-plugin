4813b57a6d312a567889eafb86d4ea837408d4a1
Kaj van der Valk <kajvandervalk@gmail.com>
2025-04-28 10:11:10 +0200

    Having a widget for every canvas
    A png file saves the state of it in the vault. Is used to redraw the canvas.

--- main.ts
+++ main.ts
@@ -4,13 +4,14 @@ import {
   StateField,
   StateEffect,
   Transaction,
+  Range,
 } from '@codemirror/state';
 import {
   ViewUpdate,
   PluginValue,
   EditorView,
   ViewPlugin,
-  WidgetType
+  WidgetType,
 } from '@codemirror/view';
 import {Facet} from "@codemirror/state"
 import {Extension} from "@codemirror/state"
@@ -21,56 +22,72 @@ import {RangeSetBuilder} from "@codemirror/state"
 
 
 
-// function getRange(node: SyntaxNode)
-let app: App | null = null
-
-
 
 class CanvasWidget extends WidgetType {
   canvas: HTMLCanvasElement | null = null;
+  filePath: string
+  app: App
+
+  constructor(filePath: string, app: App) {
+    super()
+    this.filePath = filePath
+    this.app = app
+  }
 
   toDOM(view: EditorView) {
-    const filePath = 'testfile.png'
+    const filePath = this.filePath
     this.canvas = document.createElement("canvas")
     const canvas = this.canvas
     const ctx = canvas.getContext("2d")
     if (!ctx) throw new Error("Could not get context");
     
-    const imageFile = app?.vault.getFileByPath(filePath)
-    if (!imageFile) throw new Error("Could not find the canvas file")
-    app?.vault.readBinary(imageFile)
-      .then(buffer => {
-        const blob = new Blob([buffer], { type: 'image/png' });
-        const bitmap = createImageBitmap(blob)
-        return bitmap
-      })
-      .then(bitmap => {
-        ctx.drawImage(bitmap, 0, 0) 
-      });
+    const imageFile = this.app.vault.getFileByPath(filePath)
+    if (!imageFile) {
+      console.error("Could not find the canvas file", filePath)
+      canvas.width = 700 // This is the width of the notes
+      canvas.height = 700 / 1.41421356237 // This is the width of the notes
+    } else {
+      this.app.vault.readBinary(imageFile)
+        .then(buffer => {
+          const blob = new Blob([buffer], { type: 'image/png' });
+          const bitmap = createImageBitmap(blob)
+          return bitmap
+        })
+        .then(bitmap => {
+          canvas.width = bitmap.width
+          canvas.height = bitmap.height
+          ctx.drawImage(bitmap, 0, 0) 
+        });
+    }
     
-    const canvasRect = canvas.getBoundingClientRect();
     canvas.style.width = '100%'
-    canvas.width = 700 // This is the width of the notes
-    ctx.scale(canvas.width / canvasRect.width, canvas.height / canvasRect.height)
-    ctx.fillStyle = 'red';
-    ctx.fillRect(10, 10, 100, 100)
+    canvas.style.border = '2px solid white'
+    ctx.fillStyle = 'white';
 
 
     
+    let drawing = false;
+    canvas.addEventListener('mousedown', ev => {
+      drawing = true
+    })
     
     canvas.addEventListener('mousemove', ev => {
-      ctx.fillRect(ev.offsetX, ev.offsetY, 2, 2)
+      const canvasRect = canvas.getBoundingClientRect();
+      if (!drawing) return
+      const scaleX = canvas.width / canvasRect.width
+      const scaleY = canvas.height / canvasRect.height
+      ctx.fillRect(ev.offsetX * scaleX, ev.offsetY * scaleY, 2, 2)
     })
 
-    canvas.addEventListener('click', async ev => {
-      if (app == null) return
+    canvas.addEventListener('mouseup', async ev => {
+      drawing = false;
       const buffer = await new Promise<ArrayBuffer>((resolve) => {
         canvas.toBlob((blob) => {
           if (blob === null) throw new Error("Blob was null")
           blob.arrayBuffer().then(buffer => resolve(buffer))
         }, 'image/png');  
       })
-      const files = app.vault.getFiles()
+      const files = this.app.vault.getFiles()
       let pngFile = null
       for (let file of files) if (filePath === file.path) {
         pngFile = file
@@ -78,9 +95,9 @@ class CanvasWidget extends WidgetType {
         break;
       }
       if (pngFile !== null) {
-        app.vault.modifyBinary(pngFile, buffer)
+        this.app.vault.modifyBinary(pngFile, buffer)
       } else {
-        app.vault.createBinary(filePath, buffer)
+        this.app.vault.createBinary(filePath, buffer)
       }
     })
     return canvas
@@ -92,46 +109,75 @@ class CanvasWidget extends WidgetType {
   }
 }
 
-const canvasDecoration = Decoration.replace({
-  widget: new CanvasWidget(),
-  block: true,
-})
 
 
-const canvasField = StateField.define<DecorationSet>({
-  create() {
-    const set =  Decoration.none
-    return set
-  },
-  update(canvasDecs, tr) {
-    const builder = new RangeSetBuilder<Decoration>();
-    syntaxTree(tr.state).iterate({
-      enter(node) {
-        if (node.type.name == "hmd-barelink_link") {
-          const text = tr.state.doc.sliceString(node.from, node.to)
-          if (text == "canvas") {
-            const range = canvasDecoration.range(node.from - 1, node.to + 1)
-            builder.add(range.from, range.to, range.value)
-          }
-        }
-      }
-    })
-    return builder.finish()
-  },
-  provide(field) {
-    return EditorView.decorations.from(field)
-  },
-})
+
+
 
 
 export default class ExamplePlugin extends Plugin {
   async onload() {
+    const app = this.app
+    const widgets: Map<string, CanvasWidget> = new Map()
+    const canvasField = StateField.define<DecorationSet>({
+      create() {
+        const set =  Decoration.none
+        return set
+      },
+      update(canvasDecs, tr) {
+        function getWidget(filePath: string) {
+          let widget = undefined
+          widget = widgets.get(filePath)
+          if (!widget) {
+            widget = new CanvasWidget(filePath, app) 
+            widgets.set(filePath, widget)
+            return widget
+          } else {
+            return widget
+          }
+        }
+        
+        const decos: Range<Decoration>[] = []
+        syntaxTree(tr.state).iterate({
+          enter(node) {
+            if (node.type.name == "hmd-internal-link"
+              && tr.state.doc.sliceString(node.from-3, node.from-2) == "?"
+            ) {
+              const filePath = tr.state.doc.sliceString(node.from, node.to)
+              decos.push(Decoration.widget({
+                widget: getWidget(filePath),
+                block: true,
+              }).range(node.to + 2, node.to + 2))
+            }
+          }
+        })
+        return Decoration.set(decos)
+      },
+
+      provide(field) {
+        return EditorView.decorations.from(field)
+      },
+    })
+    
     this.registerEditorExtension(canvasField);
-    app = this.app;
 
     
     this.registerMarkdownPostProcessor((element, context) => {
       // Replace all canvas tags with images
+      const filePath = 'testfile.png'
+      const file = this.app.vault.getFileByPath(filePath)
+      if (!file) return
+      console.log("Rendering image")
+      this.app.vault.readBinary(file)
+        .then(arrayBuffer => {
+          const blob = new Blob([arrayBuffer], { type: 'image/png' });
+          const imgURL = URL.createObjectURL(blob);
+    
+          // Create and insert the <img> tag
+          const img = document.createElement('img');
+          img.src = imgURL
+          element.appendChild(img)
+        })
     });
   }
 }
